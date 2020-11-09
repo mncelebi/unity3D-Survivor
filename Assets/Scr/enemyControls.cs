96fc39128c9db6fec93d39882fa6717ae9abaa62
Muhammed Nurullah Çelebi <muhammednurullahcelebi@gmail.com>
2020-11-09 18:22:32 +0300

    Add files via upload

---
+++ Assets/Scr/enemyControls.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyControls : MonoBehaviour
+{
+    private GameObject oyuncu;
+    private int zombiHealth = 3;
+    private float mesafe;
+    public GameObject kalp;
+    private int zombidenGelenPuan = 10;
+    private oyunKontrol oKontrol;
+    private AudioSource aSource;
+    private bool zombiOluyor = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        oyuncu = GameObject.Find("FPSController");
+        oKontrol = GameObject.Find("_Scripts").GetComponent<oyunKontrol>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = oyuncu.transform.position;
+        mesafe = Vector3.Distance(transform.position, oyuncu.transform.position);
+        if (mesafe < 10f)
+        {
+            if(!aSource.isPlaying)
+            aSource.Play();
+            if(!zombiOluyor)
+            GetComponentInChildren<Animation>().Play("Zombie_Attack_01");
+        }
+        else
+        {
+            if (aSource.isPlaying)
+                aSource.Stop();
+        }
+
+    }
+      private void OnCollisionEnter(Collision c)
+    {
+        if (c.collider.gameObject.tag.Equals("mermi"))
+        {
+            zombiHealth--;
+            if(zombiHealth == 0)
+            {
+                zombiOluyor = true;
+                oKontrol.PuanArttır(zombidenGelenPuan);
+                Instantiate(kalp, transform.position, Quaternion.identity);
+                GetComponentInChildren<Animation>().Play("Zombie_Death_01");
+                Destroy(this.gameObject, 1.667f);
+            }
+        }
+    }
+}
---
+++ Assets/Scr/oyunKontrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class oyunKontrol : MonoBehaviour
+{
+    public GameObject zombi;
+    private float zamanSayaci;
+    private float olusumSureci = 10f;
+    public Text puanText;
+    private int puan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zamanSayaci = olusumSureci;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zamanSayaci -= Time.deltaTime;
+        if(zamanSayaci < 0)
+        {
+            Vector3 pos = new Vector3(Random.Range(139f, 179f), 28.05f, Random.Range(248f, 317f));
+            Instantiate(zombi, pos, Quaternion.identity);
+            zamanSayaci = olusumSureci;
+        }
+    }
+    public void PuanArttır(int p)
+    {
+        puan += p;
+        puanText.text = "Puan: " + puan;
+    }
+
+    public void OyunBitti()
+    {
+        PlayerPrefs.SetInt("puan", puan);
+        SceneManager.LoadScene("OyunBitti");
+    }
+}
